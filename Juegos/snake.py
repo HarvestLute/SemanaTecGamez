e481f9d3c54b4e173a7ad3e1db099195415e5da0
Harvest <A01732792@tec,mx>
2023-05-12 23:04:52 -0600

    Modificacion de snake, comida que se mueve y no se sale de la pantalla

--- Juegos/snake.py
+++ Juegos/snake.py
@@ -10,10 +10,14 @@ Exercises
 
 from random import randrange
 from turtle import *
+#Se importa random para su uso en el movimiento de la comida
+import random
 
 from freegames import square, vector
 
 food = vector(0, 0)
+#Se añade el vector food move
+foodMove = vector(0,0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
@@ -33,6 +37,22 @@ def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
     head.move(aim)
+    food.move(foodMove)
+  #Se utiliza el vector foodmove para darle movimiento aleatoriamente a la comida
+  #Los if anidados evitan que la comida se salga de la pantalla
+    if (-200 < food.x < 190):
+        foodMove.x = random.choice([-10, 10])
+    elif (-200 > food.x):
+        food.x = -190
+    elif (food.x > 190):
+        food.x = 180
+
+    if (-200 < food.y < 190):
+        foodMove.y = random.choice([-10, 10])
+    elif (-200 > food.y):
+        food.y = -190
+    elif (food.y > 190):
+        food.y = 180
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
